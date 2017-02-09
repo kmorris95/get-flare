81420fbcd3f1596a4fc6fd98df084a7024954c7c
kmorris95 <Rocket@Karls-MBP-2.fios-router.home>
2017-02-08 22:36:52 -0500

    cleared tutorial

---
+++ Flare/src/components/login/login-signup.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+class LoginSignUp extends Component {
+    render() {
+        return(
+            <View style={styles.container}>
+                <Text>neowinfwioenfiwofinio
+                </Text>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        
+    }
+});
+
+export default LoginSignUp;
